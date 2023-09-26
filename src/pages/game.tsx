578a8eaa986ffbde980a1cf74856ed7bb59ac21f
Ludovit Mydla <Ludovit.Mydla@gjensidige.no>
2023-09-26 16:11:56 +0200

    refactor: main menu is index page, plus two more pages

---
+++ src/pages/game.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Menu() {
+  return (
+    <>
+      <main>
+        <h1>This is the GAME</h1>
+      </main>
+    </>
+  )
+}
+
---
+++ src/pages/settings.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Menu() {
+  return (
+    <>
+      <main>
+        <h1>SETTINGS page</h1>
+      </main>
+    </>
+  )
+}
