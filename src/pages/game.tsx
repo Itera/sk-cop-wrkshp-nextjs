ba1e372d17a53e9195026978406ebd6e117f68ae
Katarina Matysova <katarina.matysova@gjensidige.no>
2023-10-10 14:56:28 +0200

    feat: init game state

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+
+const BoardSize = 10;
+export type Player = 'ME' | 'OPPONENT';
+export type GameState = boolean[][]; // TODO not boolean
+
+function initBoard(): GameState {
+  const board: GameState = [];
+
+  for (let i = 0; i < BoardSize; i++) {
+    const row: boolean[] = [];
+    for (let j = 0; j < BoardSize; j++) {
+      row.push(false);
+    }
+    board.push(row);
+  }
+
+  return board;
+}
+
 export default function Menu() {
+  const [currentPlayer, setCurrentPlayer] = useState<Player>('ME');
+  const [myBoard, setMyBoard] = useState<GameState>(initBoard());
+  const [opponentsBoard, setpponentsBoard] = useState<GameState>(initBoard());
+
+
+  const handleFire = () => {
+    setCurrentPlayer((player: Player) => {
+      return player === 'ME' ? 'OPPONENT' : 'ME';
+    })
+  }
+  
   return (
     <main>
       <h1>This is the GAME</h1>
+      <p>Player: { currentPlayer }</p>
+      <button onClick={handleFire}>Fire!</button>
+      <section>
+        <h2>My board</h2>
+        [...]
+      </section>
+      <section>
+        <h2>Opponent&apos;s board</h2>
+        [...]
+      </section>
     </main>
   )
 }
