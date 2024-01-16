bee532315b66adad672ad821d273f470ac19486d
Ludovit Mydla <Ludovit.Mydla@gjensidige.no>
2024-01-16 15:58:52 +0100

    feat: multiple ships to drag

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,9 +2,9 @@ import { GameBoardState, SquareState } from "@/shared/types";
 
 export const BOARD_SIZE = 10;
 
-export function initBoard(): GameBoardState {
-  return initGrid(BOARD_SIZE, BOARD_SIZE);
+export function initBoard(fill = SquareState.Unknown): GameBoardState {
+  return initGrid(BOARD_SIZE, BOARD_SIZE, fill);
 }
-export const initGrid = (rows: number, cols: number): SquareState[][] => {
-  return Array.from(Array(rows), () => new Array(cols).fill(SquareState.Unknown));
+export const initGrid = (rows: number, cols: number, fill: SquareState): SquareState[][] => {
+  return Array.from(Array(rows), () => new Array(cols).fill(fill));
 }
